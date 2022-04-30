bf29c9dbbf999f7cb040a1663ce0cebbf3e36bde
Marcus Welander <mwthinker@yahoo.se>
2022-04-30 21:18:26 +0200

    First commit

---
+++ src/main/java/se/mwthinker/cppgenerator/CppGeneratorApplication.java
@@ -0,0 +1,13 @@
+package se.mwthinker.cppgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CppGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CppGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/mwthinker/cppgenerator/CppGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package se.mwthinker.cppgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CppGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
